0f24ae74e50d53e5af6f7acee131009137ba57d0
LuckyLuciano97 <46623336+LuckyLuciano97@users.noreply.github.com>
2023-01-13 23:45:55 +0100

    Add files via upload
    Added extra text and error checks

--- Task1/main.c
+++ Task1/main.c
@@ -20,10 +20,11 @@ if(x == y){
     } else {
         printf("Going down\n");
         for(x;x > y; x--){
-                printf("%d",x);
+            printf("%d",x);
         }
-            if(x == 0){
-        printf("You are currently in the lobby floor\nPress the Space bar and then Enter to get out of the elevator or choose another floor\n");
+
+        if(x == 0){
+            printf("You are currently in the lobby floor\nPress the Space bar and then Enter to get out of the elevator or choose another floor\n");
         } else{
             printf("You are currently on floor %d\nPress the Space bar and then Enter to get out of the elevator or choose another floor\n", x);
         }
@@ -38,59 +39,62 @@ int main() {
     int floorNum;
     bool stopElevator = false;
 
-    // printf("What floor would you like to go to?\n");
-    // scanf("%c", &floor);
 
-//P is 120 and 80
-        printf("What floor would you like to go to?\n");
+    printf("What floor would you like to go to?\nChoose between P,1,2,3,4 or 5\n");
 
-while(stopElevator == false){
+    while(stopElevator == false){
         scanf("%c", &floor);
 
-switch (floor) {
+    switch (floor) {
     case 'P':
-    floorNum = 0;
-    changeFloors(floorNum, currentFloor);
-    currentFloor = floorNum;
-    scanf("%c", &floor);
+      floorNum = 0;
+      changeFloors(floorNum, currentFloor);
+      currentFloor = floorNum;
+      scanf("%c", &floor);
     break;
-  case '1':
-  floorNum = 1;
-    changeFloors(floorNum, currentFloor);
-    currentFloor = floorNum;
-    scanf("%c", &floor);
+    case 'p':
+      floorNum = 0;
+      changeFloors(floorNum, currentFloor);
+      currentFloor = floorNum;
+      scanf("%c", &floor);
     break;
-  case '2':
-    floorNum = 2;
-    changeFloors(floorNum, currentFloor);
-    currentFloor = floorNum;
-    scanf("%c", &floor);
+    case '1':
+      floorNum = 1;
+      changeFloors(floorNum, currentFloor);
+      currentFloor = floorNum;
+      scanf("%c", &floor);
     break;
-  case '3':
-    floorNum = 3;
-    changeFloors(floorNum, currentFloor);
-    currentFloor = floorNum;
-    scanf("%c", &floor);
+    case '2':
+      floorNum = 2;
+      changeFloors(floorNum, currentFloor);
+      currentFloor = floorNum;
+      scanf("%c", &floor);
     break;
-  case '4':
-    floorNum = 4;
-    changeFloors(floorNum, currentFloor);
-    currentFloor = floorNum;
-    scanf("%c", &floor);
+    case '3':
+      floorNum = 3;
+      changeFloors(floorNum, currentFloor);
+      currentFloor = floorNum;
+      scanf("%c", &floor);
     break;
-  case '5':
-    floorNum = 5;
-    changeFloors(floorNum, currentFloor);
-    currentFloor = floorNum;
-    scanf("%c", &floor);
+    case '4':
+      floorNum = 4;
+      changeFloors(floorNum, currentFloor);
+      currentFloor = floorNum;
+      scanf("%c", &floor);
     break;
-  case ' ':
-    stopElevator = true;
-    printf("Have a nice day\n");
+    case '5':
+      floorNum = 5;
+      changeFloors(floorNum, currentFloor);
+      currentFloor = floorNum;
+      scanf("%c", &floor);
     break;
-  default:
-    printf("Please choose a floor between 0 and 5\n");
-    scanf("%c", &floor);
+    case ' ':
+      stopElevator = true;
+      printf("Have a nice day\n");
+    break;
+    default:
+      printf("Please choose a floor between 0 and 5\n");
+      scanf("%c", &floor);
     break;
 
 
@@ -100,5 +104,3 @@ switch (floor) {
 }
 
 }
-
-
